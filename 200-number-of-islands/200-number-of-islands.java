5227ed1c54007d918537a1386aad76d9c1e84932
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-05-14 11:05:14 +0530

    Time: 4 ms (66.86%), Space: 58.2 MB (14.05%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,12 +1,16 @@
 class Solution {
     public int numIslands(char[][] grid) {
         
+        //Visited array to track all visited elements
         boolean[][]  visited=new boolean[grid.length][grid[0].length]; 
         int noOfIsland=0;
         
         int row=grid.length;
         int column=grid[0].length;
         
+        
+        /*Loop will go through all the elements of matrix, but will call solUtil function,
+        only if element is not visited and is equal to 1. */
         for(int i=0;i<row;i++){
             for(int j=0;j<column;j++){
                 
@@ -20,13 +24,15 @@ class Solution {
         return noOfIsland;
     }
     
+    
+    // This implements DFS on grid algorithm
     public void solUtil(char[][] grid,int x,int y,boolean[][] visited){
         
         if(x<0 || y<0 || x>=grid.length || y>=grid[0].length){
             return;
         }
         
-        if(grid[x][y]=='0'){
+        if(grid[x][y]=='0') {
             visited[x][y]=true;
             return;
         }

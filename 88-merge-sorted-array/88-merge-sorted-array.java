c64332eeeec4c11fcacc3c9c2e23d35d36e546b3
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-05-23 14:18:40 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (99.22%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,15 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        
-         int size2=nums2.length;
+        /* size2 is the length of nums2
+           size1 is the length of nums1.
+           (PS : I didnt look at the fucntion variables, should have simply used m and n.)
+        */
+        int size2=nums2.length;
         int size1=nums1.length-size2;
 
+        /* i iterates over length m
+        j iterates over length n
+        k iterates over whole length of nums1 and stores the sorted array.
+        */
         int i=size1-1;
         int j=size2-1;
         int k=nums1.length-1;
 
-        //int[] result=new int[size1+size2];
+       
+        /* Logic : Iteration is being done from back of both arrays, and simulataneously storing the ans
+        to the back of nums1. (This is the last else part of this if-else).
+        */
         if(n==0){
             
         }
@@ -30,12 +40,16 @@ class Solution {
                 k--;
             }
 
+            /*Checks for all the left items in nums1 that haven't been iterated by i. That is, i is not
+            equalt to -1. */
             while(i>=0){
                 nums1[k]=nums1[i];
                 i--;
                 k--;
             }
             
+            /*Checks for all the left items in nums2 that haven't been iterated by j. That is, j is not
+            equalt to -1. */
             while(j>=0){
                 nums1[k]=nums2[j];
                 j--;

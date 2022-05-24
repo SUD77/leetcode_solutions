f9a002faf36160b5ece7042f68c88569a63d8dc7
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-05-24 22:22:25 +0530

    Time: 1 ms (94.31%), Space: 44.3 MB (74.86%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -6,6 +6,8 @@ class Solution {
         int j=0;
         int size=nums.length;
 
+        /* Logic : Using two pointers. Basically, i iterates over the (length of array-1).  But j is only incremented when arr[i] and arr[i+1] are not equal. Then we assign that value(arr[i+1]) to arr[j] (but after incrementing j). That's how duplicates are removed.  
+        */
         while(i<size-1){
             if(nums[i+1]==nums[i]){
                 i++;

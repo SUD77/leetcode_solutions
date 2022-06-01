7339535c8d53fd1b8071947c15e585cc9c5d22db
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-01 06:23:37 +0530

    Time: 2397 ms (5.01%), Space: 234.1 MB (5.01%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
+        /*Sorting before as to remove the conflicts, when adding duplicates 
+        in hashmap, bcoz KEY will be unique but value will change (and value is position             no)*/
         Arrays.sort(nums);
         
+        //Set is to avoid the duplicates. 
         Set<List<Integer>> resultSet=new HashSet<>();
+        
         List<List<Integer>> result=new ArrayList<>();
         HashMap<Integer,Integer> tempMap=new HashMap<>();
 
+        //Initial check for boundary cases.
         if(nums==null||nums.length==0|| nums.length==1||nums.length==2)
         {
             return result;
         }
         
+        /*Logic: Keeping one element fixed (here i), and then applying the concenpt of twoSum here. */
         for(int i=0;i<nums.length-1;i++){
 
             for(int j=i+1;j<nums.length;j++){
+                
                 List<Integer> tempList=new ArrayList<>();
+                
                 int diff= -nums[i] - nums[j];
+                
                 if(tempMap.containsKey(diff) && tempMap.get(diff)!=i && tempMap.get(diff)!=j)                 {
                     tempList.add(nums[i]);
                     tempList.add(nums[j]);
                     tempList.add(diff);
-                    Collections.sort(tempList);
+                    /*Sorting here to avoid addition of duplicates in set*/
+                    Collections.sort(tempList); 
+                    
                     resultSet.add(tempList);
                 }
                 tempMap.put(nums[i],i);
@@ -29,7 +40,7 @@ class Solution {
             }
         }
 
-
+        //Adding all the list in set in final result list. 
         result.addAll(resultSet);
         
         return result;

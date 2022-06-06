e551c589082811d9f282ecd7e24d05674476c03d
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-06 12:50:05 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (51.22%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,28 +1,22 @@
 class Solution {
     
-    int[] dp=new int[0];
-    
     public int fib(int n) {
         
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solUtil(n);
-    }
-    
-    public int solUtil(int n){
+        if(n==0) return 0;
         
-        if(dp[n]!=-1) return dp[n];
+        int[] dp=new int[n+1];
         
-        if(n==0 || n==1){
-            dp[n]=n;
-            return dp[n];
+        dp[0]=0;
+        dp[1]=1;
+        
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
         
-        int fib1=solUtil(n-1);
-        int fib2=solUtil(n-2);
-        
-        dp[n]=fib1+fib2;
-        
         return dp[n];
+       
     }
+    
+   
 }

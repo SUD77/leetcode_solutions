f6286a6fa4659fccf671102a546af700f73deebf
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-07 13:01:28 +0530

    Time: 1 ms (82.52%), Space: 43.3 MB (61.05%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -6,22 +6,26 @@ class Solution {
         
         if(n==1) return cost[n-1];
         
-        int[] dp=new int[n+1];
         
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+        int firstStep=cost[0];
+        int secondStep=cost[1];
         
         for(int i=2;i<=n;i++){
             
+            int currStep=0;
             if(i==n){
-                dp[i]=0+Math.min(dp[i-1],dp[i-2]);
+                currStep=0+Math.min(firstStep,secondStep);
             }else{
-                dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+                currStep=cost[i]+Math.min(firstStep,secondStep);
             }
+            
+            firstStep=secondStep;
+            secondStep=currStep;
+            
         }
         
         
-        return dp[n];
+        return secondStep;
     }
     
 

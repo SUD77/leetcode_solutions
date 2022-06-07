a298876b7b6ee314605c4d68ee4816d312b1ba14
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-07 06:54:00 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (20.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] dp=new int[0];
+    
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solUtil(n);
+    }
+    
+    public int solUtil(int n){
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        if(n==1 || n==2){
+            dp[n]=n;
+            return dp[n];
+        }
+        
+        int fib1=solUtil(n-1);
+        int fib2=solUtil(n-2);
+        
+        dp[n]=fib1+fib2;
+        
+        return dp[n];
+    }
+}

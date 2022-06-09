773cf86bf5bd48f9c015e961e5a2fd8621b4054a
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-10 00:32:05 +0530

    Time: 29 ms (6.95%), Space: 44.4 MB (54.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,16 +1,42 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
        
-      PriorityQueue<Integer> temp=new PriorityQueue<>();
-
-        for(int x:nums){
-            temp.add(x);
-            if(temp.size()>k){
-                temp.poll();
-            }
+      if(nums==null || nums.length==0) return Integer.MAX_VALUE;
+        return solUtil(nums,0,nums.length-1,nums.length-k);
+        
+    }
+    
+    
+    // quick select: kth smallest
+    public int solUtil(int[] nums,int start ,int end,int k){
+        
+        if(start>end) return Integer.MAX_VALUE;
+        
+        int pivot=nums[end]; //Taking A[end] as the pivot
+        int left=start;
+        
+        for(int i=start;i<end;i++){
+            if(nums[i]<=pivot) //put no < pivot to pivot's left
+                swap(nums,left++,i);
         }
+        swap(nums,left,end); //Finally, swap A[end] with A[left]
         
-        return temp.poll();
         
+        if(left==k) //ans is here
+            return nums[left];
+        else if (left < k) // check right part
+            return solUtil(nums,left+1,end,k);
+        else //check left part
+            return solUtil(nums,start,left-1,k);
+        
+        
+        
+    }
+    
+    
+    void swap(int[] A,int i, int j){
+        int temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
     }
 }

e760915b360e0e0aa9def5f13dfef84a764f2378
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-17 22:52:21 +0530

    Time: 12 ms (11.50%), Space: 43.6 MB (65.66%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,29 +1,28 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] A, int val) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         
-        //Sorting the array so that to avoid duplicates 
-        Arrays.sort(A);
-		List<List<Integer>> result=new ArrayList<>();
-        combiUtil(A,val,0,new ArrayList(),result);
-
+        Arrays.sort(candidates);
+        List<List<Integer>> result=new ArrayList<>();
+        solUtil(candidates,target,0,new ArrayList(),result);
+        
         return result;
     }
     
-    public static void combiUtil(int[] A,int remain, int start, List<Integer> currentSubset, List<List<Integer>> result){
-
+    public void solUtil(int[] A,int remain,int start,List<Integer> currentSubset, List<List<Integer>> result){
+        
         if(remain<0) return;
         
-        if(remain==0) {
+        if(remain==0){
             result.add(new ArrayList<>(currentSubset));
             return;
         }
         
-        for(int i=start;i<A.length;i++){
-            if(i>start && A[i]==A[i-1]) continue;
+        for(int i=start; i<A.length;i++){
+            
+            if(i>start && A[i]==A[i-1] ) continue;
             currentSubset.add(A[i]);
-            combiUtil(A,remain-A[i],i+1,currentSubset,result);
-            currentSubset.remove(currentSubset.size()-1);    
+            solUtil(A,remain-A[i],i+1,currentSubset,result);
+            currentSubset.remove(currentSubset.size()-1);
         }
-        
     }
 }

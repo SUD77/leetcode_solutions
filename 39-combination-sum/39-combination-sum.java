e02ea59e990d4744d7ff1f97a171d09419bdbd0e
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-06-17 22:28:56 +0530

    Time: 31 ms (6.34%), Space: 54.2 MB (5.67%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,36 +1,38 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] A, int val) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
         List<List<Integer>> result=new ArrayList<>();
-        combiUtil(A,val,0,new ArrayList(),result);
-
+        solUtil(candidates,target,0,new ArrayList<>(), result);
+        
         return result;
     }
     
-    public static void combiUtil(int[] A,int target, int currentIndex, List<Integer> currentSubset, List<List<Integer>> result){
-
-        if(currentIndex==A.length){
+    public void solUtil(int[] candidates,int target,int CurrentIndex,List<Integer> CurrentSubset,List<List<Integer>> result ){
+        
+        if(CurrentIndex==candidates.length){
             return;
         }
+        
         int sum=0;
-
-        for(Integer x:currentSubset){
+        
+        for(Integer x:CurrentSubset){
             sum+=x;
         }
-
-
+        
         if(sum>target) return;
         if(sum==target){
-            result.add(new ArrayList(currentSubset));
+            
+            result.add(new ArrayList(CurrentSubset));
             return;
         }
-       
-
-        currentSubset.add(A[currentIndex]);
-		//to add the same element in currentSubset till either sum is greater or equal
-        combiUtil(A,target,currentIndex,currentSubset,result);
-
-        currentSubset.remove(currentSubset.size()-1);
-        combiUtil(A,target,currentIndex+1,currentSubset,result);
+        
+        
+        CurrentSubset.add(candidates[CurrentIndex]);
+        solUtil(candidates,target,CurrentIndex,CurrentSubset,result);
+        
+        CurrentSubset.remove(CurrentSubset.size()-1);
+        solUtil(candidates,target,CurrentIndex+1,CurrentSubset,result);
+        
+        
     }
 }

f7c14f263495b70ffdbba9672b10f0b8de24f485
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-07-20 23:14:36 +0530

    Time: 128 ms (20.83%), Space: 68.2 MB (23.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,32 +1,59 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         
-        int sum = 0;
-    
-    for (int num : nums) {
-        sum += num;
-    }
-    
-    if ((sum & 1) == 1) {
+        
+        int sum=0;
+        
+        for(int x:nums){
+            sum+=x;    
+        }
+        
+        if(sum%2!=0) return false;
+        sum=sum/2;
+        
+        
+        
+        //dp. 
+        int dp[][]=new int[nums.length][sum+1];
+        
+        for(int[] x:dp){
+            Arrays.fill(x,-1);
+        }
+        
+      
+        int ans=solUtil(nums.length-1,sum,nums,dp);
+        
+        if(ans==1) return true;
+        
         return false;
     }
-    sum /= 2;
     
-    int n = nums.length;
-    boolean[] dp = new boolean[sum+1];
-    Arrays.fill(dp, false);
-    dp[0] = true;
-    
-    for (int num : nums) {
-        for (int i = sum; i > 0; i--) {
-            if (i >= num) {
-                dp[i] = dp[i] || dp[i-num];
-            }
-        }
-    }
-    
-    return dp[sum];
+    public int solUtil(int index,int target,int[] arr,int[][] dp){
         
+        if(dp[index][target]!=-1) return dp[index][target];
+        
+        if(target==0) return 1;
+        if(index==0){
+            
+            if(arr[0]==target) return 1;
+            else return 0;
+        } 
+            
+        
+        int notTake=solUtil(index-1,target,arr,dp);
+        
+        int take=0;
+        if(target>=arr[index]){
+            take=solUtil(index-1,target-arr[index],arr,dp);
+        }
+        
+        if(take==0 && notTake==0){
+            dp[index][target]=0;
+        }else{
+            dp[index][target]=1;
+        }
+        
+        return dp[index][target];
         
     }
 }

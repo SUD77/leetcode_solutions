8f181626e72f9c211481f20f42e28a001e1e528c
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-07-24 22:00:57 +0530

    Time: 180 ms (48.11%), Space: 39.7 MB (98.03%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,28 +1,43 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-       
+        
         int n=nums.length;
         
-        return solUtil(nums,target,n-1);
+        int totalSum=0;
+        
+        for(int x:nums){
+            totalSum+=x;
+        }
+        
+        if(totalSum-target<0) return 0;
+        
+        //newTarget can't be odd, so if this diff is not divisible by 2, return 0;
+        if((totalSum-target)%2==1) return 0;
+        
+        int newTarget=(totalSum-target)/2;
+        
+        return solUtil(n-1,nums,newTarget);
     }
     
-    public int solUtil(int[] nums,int target,int index){
+    public int solUtil(int index,int[] nums,int target){
         
-        if(index==0) {
-         
-            if(target-nums[0]==0 || nums[0]+target==0) {
-                if(nums[0]==0) return 2;
-                return 1;
-            }
-            else return 0;
+        if(index==0){
+            
+            if(target==0 && nums[0]==0) return 2;
+            
+            if(target==0 | target==nums[0]) return 1;
+            
+            return 0;
         }
- 
         
-        int add=solUtil(nums,target+nums[index],index-1);
+        int notTaken=solUtil(index-1,nums,target);
         
-        int sub=solUtil(nums,target-nums[index],index-1);
+        int taken=0;
         
+        if(nums[index]<=target){
+            taken=solUtil(index-1,nums,target-nums[index]);
+        }
         
-        return add+sub;
+        return taken+notTaken;
     }
 }

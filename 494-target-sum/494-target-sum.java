5eb604f4b07e44a14020de01ac04755b6f500eb2
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-07-24 21:40:19 +0530

    Time: 362 ms (38.62%), Space: 42.2 MB (59.95%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -3,26 +3,24 @@ class Solution {
        
         int n=nums.length;
         
-        return solUtil(nums,target,n-1,0);
+        return solUtil(nums,target,n-1);
     }
     
-    public int solUtil(int[] nums,int target,int index,int CurrSum){
-        
+    public int solUtil(int[] nums,int target,int index){
         
         if(index==0) {
          
-            
-            if(nums[0]+CurrSum==target || CurrSum-nums[0]==target) {
-               
+            if(target-nums[0]==0 || nums[0]+target==0) {
                 if(nums[0]==0) return 2;
                 return 1;
             }
             else return 0;
         }
+ 
         
-        int add=solUtil(nums,target,index-1,CurrSum+nums[index]);
+        int add=solUtil(nums,target+nums[index],index-1);
         
-        int sub=solUtil(nums,target,index-1,CurrSum-nums[index]);
+        int sub=solUtil(nums,target-nums[index],index-1);
         
         
         return add+sub;

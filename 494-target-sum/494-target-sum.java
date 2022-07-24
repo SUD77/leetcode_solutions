2b58e53a68fd4fb2c3a05c041d287ac3901fb416
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-07-24 22:21:10 +0530

    Time: 7 ms (87.70%), Space: 43.1 MB (48.69%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -3,6 +3,9 @@ class Solution {
         
         int n=nums.length;
         
+       
+       
+        
         int totalSum=0;
         
         for(int x:nums){
@@ -13,14 +16,23 @@ class Solution {
         
         //newTarget can't be odd, so if this diff is not divisible by 2, return 0;
         if((totalSum-target)%2==1) return 0;
-        
         int newTarget=(totalSum-target)/2;
         
-        return solUtil(n-1,nums,newTarget);
+        //Making dp  array with newTarget
+        
+         int dp[][]=new int[n][newTarget+1];
+        
+        for(int[] x:dp){
+             Arrays.fill(x,-1);
+        }
+        
+        
+        
+        return solUtil(n-1,nums,newTarget,dp);
     }
     
-    public int solUtil(int index,int[] nums,int target){
-        
+    public int solUtil(int index,int[] nums,int target,int[][] dp){
+ 
         if(index==0){
             
             if(target==0 && nums[0]==0) return 2;
@@ -30,14 +42,16 @@ class Solution {
             return 0;
         }
         
-        int notTaken=solUtil(index-1,nums,target);
+         if(dp[index][target]!=-1) return dp[index][target];
+        
+        int notTaken=solUtil(index-1,nums,target,dp);
         
         int taken=0;
         
         if(nums[index]<=target){
-            taken=solUtil(index-1,nums,target-nums[index]);
+            taken=solUtil(index-1,nums,target-nums[index],dp);
         }
         
-        return taken+notTaken;
+        return dp[index][target]=taken+notTaken;
     }
 }

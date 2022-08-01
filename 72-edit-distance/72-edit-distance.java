f7e37be800d8f92ed8c148c91fe38bf33d04d0a4
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-08-01 21:43:32 +0530

    Time: 9 ms (54.41%), Space: 44.8 MB (59.73%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -4,36 +4,31 @@ class Solution {
         int len1=word1.length();
         int len2=word2.length();
         
-        int[][] dp=new int[len1][len2];
+        int[][] dp=new int[len1+1][len2+1];
         
-        for(int[] x:dp){
-            Arrays.fill(x,-1);
+        for(int i=0;i<=len1;i++){
+            dp[i][0]=i;
         }
         
-        return solUtil(word1,word2,len1-1,len2-1,dp);
+        for(int j=0;j<=len2;j++){
+            dp[0][j]=j;
+        }
+        
+        
+        for(int i=1;i<=len1;i++){
+            
+            for(int j=1;j<=len2;j++){
+                
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                
+                else dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+            }
+                
+        }
+        
+        return dp[len1][len2];
     }
     
-    public int solUtil(String s1,String s2,int index1,int index2,int[][] dp){
-        
-        //s1 is exhausted
-        if(index1<0) return index2+1;  
-        
-        //s2 is exhausted
-        if(index2<0) return index1+1;
-        
-        if(dp[index1][index2]!=-1) return dp[index1][index2];
-        
-        if(s1.charAt(index1)==s2.charAt(index2)) 
-            return dp[index1][index2]=solUtil(s1,s2,index1-1,index2-1,dp);
-        
-        
-        //if not equal then do insert,replace,delete
-        
-        int insert=1+solUtil(s1,s2,index1,index2-1,dp);
-        int delete=1+solUtil(s1,s2,index1-1,index2,dp);
-        int replace=1+solUtil(s1,s2,index1-1,index2-1,dp);
-        
-        return dp[index1][index2]=Math.min(insert, Math.min(delete,replace));
-        
-    }
 }

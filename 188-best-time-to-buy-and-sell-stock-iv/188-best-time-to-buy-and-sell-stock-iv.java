f4b04b01d1f9433872512afbe63db5482cb8ec7e
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-08-02 23:48:43 +0530

    Time: 2 ms (91.10%), Space: 41.7 MB (88.95%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,39 +1,35 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         
-        int n=prices.length;
-       
-        int[][][] dp=new int[n][2][k+1];
         
+         int n=prices.length;
+        
+        int[][][] dp=new int[n+1][2][k+1];
+        
+        
+        /* Doint all values to zero will ensure that our base cases are captured here
+        i.e of dp[0..to..n][0 or 1][0] = 0 and similarly for when index is n, everything will be 0. Look at strivers video, if any doubt*/
         for(int[][] x:dp){
             for(int[] y:x){
-                Arrays.fill(y,-1);
+                Arrays.fill(y,0);
             }
         }
         
-        return solUtil(prices,n,0,1,k,dp);
-    }
-    
-     public int solUtil(int[] prices,int n,int index,int buy,int transaction,int[][][] dp){
-        
-        
-        
-        if(transaction==0) return 0;
-        if(index==n) return 0;
-        
-        if(dp[index][buy][transaction]!=-1) return dp[index][buy][transaction];
-        
-        int profit=0;
-        
-        if(buy==1){
+        for(int index=n-1;index>=0;index--){
             
-            profit=Math.max(-prices[index]+solUtil(prices,n,index+1,0,transaction,dp),solUtil(prices,n,index+1,1,transaction,dp));
-        }else{
-            
-            profit=Math.max(prices[index] + solUtil(prices,n,index+1,1,transaction-1,dp) , solUtil(prices,n,index+1,0,transaction,dp));
+            for(int buy=0;buy<=1;buy++){
+                
+                for(int cap=1;cap<=k;cap++){
+                    
+                    if(buy==1){
+                        dp[index][buy][cap]=Math.max(-prices[index]+dp[index+1][0][cap],dp[index+1][1][cap]);
+                    } else {
+                        dp[index][buy][cap]= Math.max(prices[index] + dp[index+1][1][cap-1] , dp[index+1][0][cap]);
+                    }
+                }
+            }
         }
         
-        return dp[index][buy][transaction]=profit;
-        
+        return dp[0][1][k];
     }
 }

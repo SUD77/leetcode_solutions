90eba76fbae2fac8782cd5ce747984bbf5ce2090
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-08-21 13:59:40 +0530

    Time: 72 ms (100.00%), Space: 64.7 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.java
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.java
@@ -2,16 +2,24 @@ class Solution {
     public String largestPalindromic(String num) {
         
          StringBuilder sb=new StringBuilder();
-
+            
+        // To store count of digits
          TreeMap<Character,Integer> map=new TreeMap<>();
 
+        
+        /* List too :  for stroing key value pair whose count are divisible by 2.
+            List onn: for stroing other counts
+        */
          List<Map.Entry<Character,Integer>> too=new ArrayList<>();
          List<Map.Entry<Character,Integer>> onn=new ArrayList<>();
 
+        //Putting values in map
          for(int i=0;i<num.length();i++){
              map.put(num.charAt(i),map.getOrDefault(num.charAt(i),0)+1);
          }
 
+        
+        //From map, putting values in list
          for(Map.Entry<Character,Integer> ele:map.entrySet()){
 
 
@@ -30,10 +38,12 @@ class Solution {
          }
 
 
+        //Sorting both list by key values. In decreasing order.
         Collections.sort(too,(i1,i2)-> i2.getKey().compareTo(i1.getKey()));
         Collections.sort(onn,(i1,i2)-> i2.getKey().compareTo(i1.getKey()));
 
 
+        //This will form the half palindrome list. And this only goes thorugh list too. 
         for(Map.Entry<Character,Integer> ele:too){
 
             int t=ele.getValue()/2;
@@ -56,16 +66,21 @@ class Solution {
         }
 
         
+        //Reversing the sb String
         StringBuilder revSB=new StringBuilder(sb);
 
+        //From onn list, we add the highest key in sb
         if(onn.size()>0){
             sb.append(onn.get(0).getKey());
         }
 
 
+        //appendig the reverse of sb
         sb.append(revSB.reverse());
 
+        //If sb is empty, return "0". This is for test cases like "0000"
         if(sb.toString().length()==0) return "0";
+        
          return sb.toString();
     }
 }

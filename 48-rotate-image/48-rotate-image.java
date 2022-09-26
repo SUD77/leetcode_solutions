0b13ab4dd0baf87b1454c605bbd86a834a39ff3c
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-09-27 01:06:24 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (12.55%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,38 +1,39 @@
 class Solution {
     public void rotate(int[][] matrix) {
         
+        transpose(matrix);
+        exchangeTheColumns(matrix);
         
-        /*
-         * clockwise rotate
-         * first reverse up to down, then swap the symmetry 
-         * 1 2 3     7 8 9     7 4 1
-         * 4 5 6  => 4 5 6  => 8 5 2
-         * 7 8 9     1 2 3     9 6 3
-        */
+    }
+    
+    public void transpose(int[][] matrix){
         
-        int start=0;
-        int end=matrix.length-1;
-        
-        while(start<end){
-            
-            int[] temp=matrix[start];
-            matrix[start]=matrix[end];
-            matrix[end]=temp;
-            start++;
-            end--;
-        }
-        
-        for(int i=0;i<matrix.length;i++){
-            
-            for(int j=i+1; j<matrix[i].length;j++){
-                
+        for(int i=0;i< matrix.length;i++){
+
+            for(int j=i;j<matrix[0].length;j++){
+
                 int temp=matrix[i][j];
-                
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
-            
+    }
+    
+    public void exchangeTheColumns(int[][] matrix){
         
+         for(int i=0;i<matrix.length;i++){
+
+            int j=0;
+            int k=matrix[0].length-1;
+
+            while(j<k){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][k];
+                matrix[i][k]=temp;
+
+                j++;
+                k--;
+            }
+        }
     }
 }

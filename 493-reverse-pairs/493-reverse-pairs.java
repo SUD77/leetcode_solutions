f72a0b60c7fc47203360784ddb1abaf02a0565a2
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-09-30 13:19:53 +0530

    Time: 166 ms (30.79%), Space: 119.7 MB (10.05%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.java
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -1,17 +1,13 @@
 class Solution {
     public int reversePairs(int[] nums) {
-        
         return mergeSort(nums,0,nums.length-1);
     }
     
     public int mergeSort(int[] nums,int low,int high){
         
         if(low>=high) return 0;
-        
         int mid=(low+high)/2;
-        
         int inv=mergeSort(nums,low,mid);
-        
         inv+=mergeSort(nums,mid+1,high);
         inv+=merge(nums,low,mid,high);
         
@@ -19,48 +15,42 @@ class Solution {
     }
     
     public int merge(int[] nums,int low,int mid,int high){
+        int count=0;
         
-        int cnt=0;
         int j=mid+1;
         
         for(int i=low;i<=mid;i++){
-            
-            while(j<=high && nums[i]>(2*(long) nums[j])){
+            while(j<=high && nums[i]> (2 * (long) nums[j])){
                 j++;
             }
-            
-            cnt+=(j-(mid+1));
+            count+=(j-(mid+1));
         }
-    
-    
-    ArrayList<Integer> temp=new ArrayList<>();
-    
-    int left= low ;
-    int right=mid+1;
-    
-    while(left<=mid && right<=high){
         
-        if(nums[left]<=nums[right]){
+        ArrayList<Integer> temp=new ArrayList<>();
+        
+        int left=low, right = mid+1;
+        
+        while(left<=mid && right<=high){
+            
+            if(nums[left]<=nums[right]){
+                temp.add(nums[left++]);
+            }else{
+                temp.add(nums[right++]);
+            }
+        }
+        
+        while(left<=mid){
             temp.add(nums[left++]);
-        }else{
+        }
+        
+        while(right<=high){
             temp.add(nums[right++]);
         }
-    }
-    
-    while(left<=mid){
-        temp.add(nums[left++]);
-    }
-    
-    while(right<=high){
-        temp.add(nums[right++]);
-    }
-    
-    
-    for(int i=low;i<=high;i++){
         
-        nums[i]=temp.get(i-low);
-    }
-    
-    return cnt;
+        for(int i=low;i<=high;i++){
+            nums[i]=temp.get(i-low);
+        }
+        
+        return count;
     }
 }

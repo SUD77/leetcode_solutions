5bad45545ebfec49482d75a35e1ae492176739be
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-10-08 18:08:49 +0530

    Time: 28 ms (15.21%), Space: 54.5 MB (37.37%) - LeetHub

---
+++ 477-total-hamming-distance/477-total-hamming-distance.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        
+        int ans=0;
+        
+        for(int i=0;i<32;i++){
+            
+            int count1=0;
+            int count0=0;
+            
+            for(int j=0;j<nums.length;j++){
+                
+                if(((1<<i) & nums[j])==0) count0++;
+                else count1++;
+            }
+            
+            ans+=count1*count0;
+        }
+        
+        return ans;
+    }
+}

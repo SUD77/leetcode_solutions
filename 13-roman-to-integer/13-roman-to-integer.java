596350c6a0968acbdc7a62ed7c003cfde92a912f
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-10-17 14:38:32 +0530

    Time: 13 ms (37.88%), Space: 46.8 MB (28.98%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,46 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
         
-        int nums[]=new int[s.length()];
-        for(int i=0;i<s.length();i++){
+        Map<Character,Integer> map=new HashMap<>();
+        
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        
+        int result=map.get(s.charAt(s.length()-1));
+        
+        
+        for(int i=s.length()-2;i>=0;i--){
             
-            switch(s.charAt(i)){
-                    
-                case 'M':
-                    nums[i]=1000;
-                    break;
-                case 'D':
-                    nums[i]=500;
-                    break;
-                case 'C':
-                    nums[i]=100;
-                    break;
-                case 'L':
-                    nums[i]=50;
-                    break;
-                case 'X':
-                    nums[i]=10;
-                    break;
-                case 'V':
-                    nums[i]=5;
-                    break;
-                case 'I':
-                    nums[i]=1;
-                    break;
-            }
-        }
-        
-        int sum=0;
-        
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]){
-                sum-=nums[i];
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                result-=map.get(s.charAt(i));
             }else{
-                sum+=nums[i];
+                result+=map.get(s.charAt(i));
             }
         }
         
-        return sum+nums[nums.length-1];
-        
+        return result;
     }
 }

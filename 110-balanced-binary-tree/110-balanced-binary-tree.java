e0bda5730b8ad4ba3f996c0ab48032d1013a3a4a
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-10-20 14:05:59 +0530

    Time: 1 ms (99.21%), Space: 44.7 MB (25.14%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -15,22 +15,21 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        
-        return dfsHeight(root)!=-1;
+        int ans = maxHeight(root);
+        if(ans == Integer.MAX_VALUE) return false;
+        return true;
     }
-    
-    int dfsHeight(TreeNode root){
+    private static int maxHeight(TreeNode root){
+        if(root == null ) return 0;
+
+        int lh = maxHeight(root.left);
+        if(lh==Integer.MAX_VALUE) return Integer.MAX_VALUE;
         
-        if(root==null) return 0;
-        
-        int leftHeight=dfsHeight(root.left);
-        if(leftHeight==-1) return -1;
-        
-        int rightHeight=dfsHeight(root.right);
-        if(rightHeight==-1) return -1;
-        
-        if(Math.abs(leftHeight-rightHeight)>1) return -1;
-        
-        return Math.max(leftHeight,rightHeight)+1;
+        int rh = maxHeight(root.right);
+        if(rh==Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+        if(Math.abs(lh - rh) > 1) return Integer.MAX_VALUE;
+
+        return 1 + Math.max(lh , rh);
     }
 }

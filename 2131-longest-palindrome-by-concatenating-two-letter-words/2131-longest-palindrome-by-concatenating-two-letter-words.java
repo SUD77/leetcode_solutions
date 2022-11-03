bcc084b9b7a78e41b8a59f1c3442e9b50afc9c0e
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-11-03 09:12:45 +0530

    Time: 127 ms (68.26%), Space: 59.4 MB (85.59%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,31 @@
+/*
+https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/discuss/1675693/JavaPython-3-One-pass-w-brief-explanation-and-analysis.
+*/
+
+class Solution {
+    public int longestPalindrome(String[] words) {
+        
+        Map<String,Integer> wordsNotPaired=new HashMap<>();
+        
+        int pairs=0;
+        int symmetricWords=0;
+        
+        for(String w:words){
+            
+            String reverse=new StringBuilder(w).reverse().toString();
+            
+            if(wordsNotPaired.getOrDefault(reverse,0)>0){
+                ++pairs;
+                wordsNotPaired.merge(reverse,-1,Integer::sum);
+                symmetricWords-=w.charAt(0)==w.charAt(1) ? 1:0;
+            }else{
+                
+                wordsNotPaired.merge(w, 1,Integer::sum);
+                
+                symmetricWords+=w.charAt(0)==w.charAt(1) ? 1:0;
+            }
+        }
+        
+        return 4 * pairs + (symmetricWords > 0 ? 2 : 0);
+    }
+}

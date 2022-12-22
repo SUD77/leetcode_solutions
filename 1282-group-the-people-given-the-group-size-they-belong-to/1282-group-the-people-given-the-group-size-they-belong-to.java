abbf1bc7e925b6f57a03a0ec3f2d502d50437e3e
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-12-22 22:12:11 +0530

    Time: 5 ms (92.02%), Space: 43.1 MB (81.62%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -3,19 +3,31 @@ class Solution {
         
         List<List<Integer>> ans=new ArrayList<>();
 
+        
+        //temp arary to store the elements in there respective group sizes
         List<List<Integer>> temp=new ArrayList<>();
 
-
+     
+        //Assigning each group size no a Empty arrayList
         for(int j=0;j<= groupSizes.length;j++){
             temp.add(j,new ArrayList<>());
         }
 
+        
+        //Putting elements in there repsective group size
         for(int i=0;i<groupSizes.length;i++){
-
 
             temp.get(groupSizes[i]).add(i);
         }
 
+        
+        /*
+        Here, we will fill ans list.
+        If 6 people are in group size of 3, so in if condition,
+        we create 2 sublist of 3 size and add them in ans.
+        Simliarly for other sizes. 
+        
+        */
         for(int i=0;i<temp.size();i++){
 
             if(temp.get(i).size() > i){
@@ -28,13 +40,10 @@ class Solution {
                     ans.add(temp.get(i).subList(q,q+i));
                     q=q+i;
                 }
-            }else{
+            }else {
                 if(!temp.get(i).isEmpty())ans.add(temp.get(i));
             }
         }
-
-
-
 
         return ans;
     }

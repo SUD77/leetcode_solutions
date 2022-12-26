e2f4e8a2dc28c3ed435264cacda1eb31c21c3215
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-12-26 12:23:07 +0530

    Time: 258 ms (17.54%), Space: 42.9 MB (85.72%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    public boolean canJump(int[] nums) {
+        
+        int[] dp=new int[nums.length];
+        
+        //Arrays.fill(dp,-1);
+        
+        return solUtil(nums,0,dp);
+    }
+    
+    public boolean solUtil(int[] nums,int index,int[] dp){
+        
+        if(index==nums.length-1) return true;
+
+        if(index>=nums.length || nums[index]==0 || dp[index]==-1) return false;
+        
+        if(dp[index]==1) return true;
+        
+        // int maxReach=index+nums[index];
+ 
+        for(int i=index+nums[index];i>=index+1;i--)
+        {
+            
+            if(solUtil(nums,i,dp)){
+                dp[index]=1;
+                return true;
+            }
+                
+        }
+        
+        dp[index]=-1;
+        return false; 
+        
+        
+    }
+}

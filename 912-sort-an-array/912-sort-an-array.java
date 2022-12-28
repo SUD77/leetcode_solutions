3b810a2377c5f116a354c8a1d23f8361af77e068
SUD77 <jaiswalsudhanshu20@gmail.com>
2022-12-28 20:11:21 +0530

    Time: 95 ms (30.21%), Space: 51.3 MB (98.63%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+        for(int x:nums){
+            pq.add(x);
+        }
+        
+
+        int i=0;
+
+        while (!pq.isEmpty()){
+
+            nums[i++]=pq.poll();
+
+        }
+        return nums;
+    }
+}

1776877e66ab3720405e5cd961fd7bf61069b679
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-02-05 12:08:31 +0530

    Time: 36 ms (100.00%), Space: 54.9 MB (75.00%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        
+        int left=1;
+        int right= (int)1e9;
+        
+        while(left < right) {
+            
+            int mid=( left + right ) >> 1;
+            
+            if(isPossible(mid,nums,k))
+                right=mid;
+            else
+                left=mid+1;
+        }
+        
+        return left;
+    }
+    
+    public boolean isPossible(int mid,int[] nums,int k){
+        
+        int lastStealIndex=-2; // cant have -1 
+        int houseRobbed=0;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            if(nums[i] > mid) continue;
+            if(i == lastStealIndex + 1) continue;
+            
+            houseRobbed++;
+            lastStealIndex=i;
+        }
+        
+        return (houseRobbed >= k);
+        
+    }
+}

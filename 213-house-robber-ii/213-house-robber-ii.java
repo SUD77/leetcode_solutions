37b39db8ee5179945a4b623ea186491c29aa74af
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-02-05 11:21:15 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (66.15%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -11,25 +11,7 @@ class Solution {
         
         int n=nums.length;
         
-        solUtil(nums,n-1,1,dp);
-        
-        //System.out.println(Arrays.toString(dp));
-        
-        solUtil(nums,n-2,0,dp2);
-        
-        //System.out.println(Arrays.toString(dp));
-        
-        int maxV=Integer.MIN_VALUE;
-        
-        for(int x:dp){
-            maxV=Math.max(maxV,x);
-        }
-        
-        for(int x:dp2){
-            maxV=Math.max(maxV,x);
-        }
-        
-        return maxV;
+        return Math.max(solUtil(nums,n-1,1,dp),solUtil(nums,n-2,0,dp2));
     }
     
     public int solUtil(int[] nums,int start,int end,int[] dp){
@@ -40,12 +22,9 @@ class Solution {
         
         if(dp[start]!=-1) return dp[start];
         
-        //System.out.println(start);
-        
         int pick=nums[start] + solUtil(nums,start-2,end,dp);
         int notPick=solUtil(nums,start-1,end,dp);
         
-        //System.out.println(pick);
         return dp[start]=Math.max(pick,notPick);
     }
 }

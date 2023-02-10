43d39cc405a8ccec671e26771c964a18cdac6801
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-02-10 20:54:34 +0530

    Time: 15 ms (50.33%), Space: 45.8 MB (53.64%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        
+        int n=mat.length;
+        int m=mat[0].length;
+        
+        boolean[][] vis=new boolean[n][m];
+        int[][] dis=new int[n][m];
+        
+        Queue<Node> q=new LinkedList<>();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(mat[i][j]==0){
+                    q.add(new Node(i,j,0));
+                    vis[i][j]=true;
+                }else{
+                    vis[i][j]=false;
+                }
+            }
+        }
+        
+        int[] delRow={-1,0,1,0};
+        int[] delCol={0,1,0,-1};
+        
+        while(!q.isEmpty()){
+            
+            int row=q.peek().first;
+            int col=q.peek().second;
+            int steps=q.peek().third;
+            
+            q.remove();
+            
+            dis[row][col]=steps;
+            
+            for(int i=0;i<4;i++) {
+                
+                int nRow=row+delRow[i];
+                int nCol=col+delCol[i];
+                
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol]){
+                    vis[nRow][nCol]=true;
+                    q.add(new Node(nRow,nCol,steps+1));
+                }
+            }
+        }
+        
+        return dis;
+    }
+    
+    class Node{
+        int first;
+        int second;
+        int third;
+        
+        Node(int first,int second,int third){
+            this.first=first;
+            this.second=second;
+            this.third=third;
+        }
+    }
+}

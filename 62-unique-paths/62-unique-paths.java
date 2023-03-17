acaf7d2add01076b78c1c0a5351a4f6286a06b06
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-03-18 02:00:39 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (54.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,27 +1,32 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int[][] dp=new int[m][n];
+        int[] prev=new int[n];
         
-        dp[m-1][n-1]=1;
-        
-        //filling up last row
-        for(int i=0;i<n;i++){
-            dp[m-1][i]=1;
-        }
-        
-        //filling up last column
-        for(int i=0;i<m;i++){
-            dp[i][n-1]=1;
-        }
-        
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j]=dp[i+1][j] + dp[i][j+1];
+        for(int i=m-1;i>=0;i--){
+            
+            int[] cur=new int[n];
+            
+            for(int j=n-1;j>=0;j--){
+                
+                if(i==m-1 && j==n-1) cur[j]=1;
+                else{
+                    int down=0;
+                    int right=0;
+                    
+                    if(i+1<m) down=prev[j];
+                    if(j+1<n) right=cur[j+1];
+                    
+                    cur[j]=down+right;
+                }
+                
+                
             }
+            
+            prev=cur;
         }
         
-        return dp[0][0];
+        return prev[0];
     }
     
     

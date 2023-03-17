1322cc500800df0d309e900282a598341e7578d3
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-03-17 10:50:24 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (34.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,7 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int[][] dp=new int[m+1][n+1];
+        int[][] dp=new int[m][n];
         
         for(int[] x:dp){
             Arrays.fill(x,-1);
@@ -12,15 +12,16 @@ class Solution {
     
     public int solUtil(int m,int n,int i,int j,int[][] dp){
         
-        if(i>=m || j>=n) return 0;
+        if(i>=m || j>=n ) return 0;
+        
+        if(i==m-1 && j==n-1) return 1;
         
         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(i==m-1 && j==n-1) return dp[i][j]=1;
-            
-        int down =solUtil(m,n,i+1,j,dp);
-        int right=solUtil(m,n,i,j+1,dp);
+        int down=solUtil(m,n,i,j+1,dp);
+        int left=solUtil(m,n,i+1,j,dp);
+       
         
-        return dp[i][j]=down+right;
+        return dp[i][j]=down+left;
     }
 }

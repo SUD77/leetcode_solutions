082a3122213cd5675fed88eac2defff47ae7e021
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-03-27 00:40:49 +0530

    Time: 107 ms (23.70%), Space: 54.8 MB (27.56%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,42 +1,60 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         
-        
         int sum=0;
         
         for(int x:nums){
             sum+=x;    
         }
         
-        if(sum%2!=0) return false;
-        sum=sum/2;
-        
-        
-        
-        //dp. 
-        boolean dp[][]=new boolean[nums.length+1][sum+1];
-        
-        for(int i=0;i<=nums.length;i++){
-            
-            for(int j=0;j<=sum;j++){
-                
-                if(i==0 && j!=0){
-                    dp[i][j]=false;
-                }else if(j==0){
-                    dp[i][j]=true;
-                }else{
-                    
-                    if(j>=nums[i-1]){
-                        dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                    }else{
-                        dp[i][j]=dp[i-1][j];
-                    }
-                }
-            }
+        //if its odd, return false
+        if(sum%2!=0){
+            return false;
         }
-    
-        return dp[nums.length][sum];
+        
+        int target=sum/2;
+        int n=nums.length;
+        
+        int[][] dp=new int[n][target+1];
+        
+        for(int[] x:dp){
+            Arrays.fill(x,-1);    
+        }
+        
+        
+        int ans=solUtil(nums,n-1,target,dp);
+        
+        if(ans==1) return true;
+        return false;
     }
     
+    public int solUtil(int[] nums,int index,int target,int[][] dp){
+        
+        if(dp[index][target]!=-1) return dp[index][target];
+        
+        if(target==0) return 1;
+        
+        if(index==0){
+            if(target-nums[index]==0) return 1;
+            else return 0;
+        }
+        
+        
+
+        
+        int notPick=solUtil(nums,index-1,target,dp);
+        
+        int pick=0;
+        
+        if(target>=nums[index]) 
+            pick=solUtil(nums,index-1,target-nums[index],dp);
   
+        
+        if(pick==0 && notPick==0){
+            dp[index][target]=0;
+        }else{
+            dp[index][target]=1;
+        }
+        return dp[index][target];
+    }
 }

c33008b3b83115e7a4308c0320552727fdb70646
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-01 19:46:12 +0530

    Time: 39 ms (13.82%), Space: 51.4 MB (10.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,71 +1,30 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-     
+        
         int len1=text1.length();
         int len2=text2.length();
         
+        int[][] dp=new int[len1][len2];
         
-        int[][] dp=new int[len1+1][len2+1];
-
-        for(int i=0;i<len2;i++){       
-                dp[0][i]=0; 
-        }
-          
-        for(int i=0;i<len1;i++){
-                dp[i][0]=0;
+        for(int[] x:dp){
+            Arrays.fill(x,-1);
         }
         
-        
-        for(int i=1;i<=len1;i++){
-            
-            
-            for(int j=1;j<=len2;j++){
-                
-                if(text1.charAt(i-1)==text2.charAt(j-1)) 
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-
-            }
-        }
-        
-        
-        
-        //from here Printing of lcs logic is implemented 
-        
-        int tempLen = dp[len1][len2];
-        int i=len1;
-        int j=len2;
-        
-        int index=tempLen-1;
-        String str="";
-        
-        for(int k=1;k<=tempLen;k++){
-            str+="$";
-        }
-        
-        StringBuilder ss=new StringBuilder(text1);
-        StringBuilder str2=new StringBuilder(str);
-        
-        while(i>0 && j>0){
-            
-            if(ss.charAt(i-1)== text2.charAt(j-1)){
-                str2.setCharAt(index,ss.charAt(i-1));
-                index--;
-                i--;
-                j--;
-            }else if(ss.charAt(i-1) > text2.charAt(j-1)){
-                i--;
-            }else j--;
-        }
-        
-        System.out.println(str2);
-        
-        
-        return dp[len1][len2];
+        return solUtil(text1,text2,0,0,dp);
     }
-    
-    
+
+    public int solUtil(String text1, String text2,int i,int j,int[][] dp){
+        
+        if(i==text1.length() || j==text2.length()){
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        
+        if(text1.charAt(i)==text2.charAt(j)){
+            return 1+solUtil(text1,text2,i+1,j+1,dp);
+        }
+        
+        return dp[i][j]= Math.max(solUtil(text1,text2,i+1,j,dp),solUtil(text1,text2,i,j+1,dp)); 
+    }
 }

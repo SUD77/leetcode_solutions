ed9ff928b1d413a266786efb51c61aa44d99e54b
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-02 11:14:27 +0530

    Time: 6 ms (55.99%), Space: 42.1 MB (60.11%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -1,33 +1,41 @@
+//Space optimization
+// TC - O(n*amount)
+// Sc - O(amount)
+
 class Solution {
     public int change(int amount, int[] coins) {
         
         int n=coins.length;
         
-        int[][] dp=new int[n][amount+1];
+        int[] prev=new int[amount+1];
+        
         
         for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0) dp[0][i]=1;
-            else dp[0][i]=0;
+            if(i%coins[0]==0) prev[i]=1;
+            else prev[i]=0;
         }
         
         for(int i=1;i<n;i++){
             
+            int[] curr=new int[amount+1];
+            
             for(int j=0;j<=amount;j++){
                 
-                int notPick=dp[i-1][j]; 
+                int notPick=prev[j]; 
         
                 int pick=0;
 
                 if(j >=coins[i]){
-                    pick=dp[i][j-coins[i]]; 
+                    pick=curr[j-coins[i]]; 
                 }
 
-                dp[i][j]=pick+notPick;
+                curr[j]=pick+notPick;
                 
             }
+            prev=curr;
         }
         
-        return dp[n-1][amount];
+        return prev[amount];
     }
     
 }

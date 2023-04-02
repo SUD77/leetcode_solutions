e39b9b44ca0d54ebc54e215eadfc063208a212dd
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-02 12:27:23 +0530

    Time: 123 ms (9.92%), Space: 61.7 MB (61.57%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/successful-pairs-of-spells-and-potions/discuss/2138875/JavaC%2B%2BPython-Straignt-Forward-with-Explantion
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+		Arrays.sort(potions);
+		TreeMap<Long, Integer> map = new TreeMap<>();
+        map.put(Long.MAX_VALUE, potions.length);
+		for (int i = potions.length - 1; i >= 0; i--) {
+			map.put((long) potions[i], i);
+		}
+		for (int i = 0; i < spells.length; i++) {
+            long need = (success + spells[i] - 1) / spells[i];
+			spells[i] = potions.length - map.ceilingEntry(need).getValue();
+		}
+		return spells;
+	}
+}

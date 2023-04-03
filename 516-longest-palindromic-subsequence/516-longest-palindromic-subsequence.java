e9eccbbcf2cef3b1945624a4c01dc51b68574511
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-03 19:49:51 +0530

    Time: 71 ms (47.75%), Space: 52.3 MB (19.03%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,38 +1,33 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         
-        
-        //Start  : Creating reverse of s
-        StringBuffer tempS=new StringBuffer(s);
-        tempS.reverse();
-        String s2=tempS.toString();
-        //End 
+        //reversing the String 
+        StringBuilder sb=new StringBuilder(s);
+        String s2=sb.reverse().toString();
         
         int len1=s.length();
         int len2=s2.length();
         
         
-        int[] prev=new int[len2+1];
+        int[][] dp=new int[len1][len2];
         
-        int[] curr=new int[len2+1];
-       
-        
-        for(int i=1;i<=len1;i++){
-            
-            for(int j=1;j<=len2;j++){
-                
-                if(s.charAt(i-1)==s2.charAt(j-1)){
-                    curr[j]=1+prev[j-1];
-                }else{
-                    curr[j]=Math.max(prev[j],curr[j-1]);
-                }
-        
-            }
-            prev=(int[]) (curr.clone());
+        for(int[] x:dp){
+            Arrays.fill(x,-1);    
         }
-               
-        return prev[len2];
-
+        
+        return solUtil(s,s2,len1-1,len2-1,dp);
     }
     
+    public int solUtil(String s1,String s2,int i,int j,int[][] dp){
+        
+        if(i<0 || j<0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s1.charAt(i)==s2.charAt(j)){
+            return 1+solUtil(s1,s2,i-1,j-1,dp);
+        }
+        
+        return dp[i][j]=Math.max(solUtil(s1,s2,i-1,j,dp),solUtil(s1,s2,i,j-1,dp));
+    }
 }

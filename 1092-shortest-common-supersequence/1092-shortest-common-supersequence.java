4b679f2f9206d2755b24004f3e6facc6f3e5ffb8
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-04 00:30:09 +0530

    Time: 21 ms (35.62%), Space: 45.7 MB (91.66%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,7 +1,6 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         
-        
         int len1=str1.length();
         int len2=str2.length();
         
@@ -15,22 +14,20 @@ class Solution {
             dp[0][i]=0;
         }
         
-        
-        for(int idx1=1;idx1<=len1;idx1++){
+        for(int i=1;i<=len1;i++){
             
-            for(int idx2=1;idx2<=len2;idx2++){
+            for(int j=1;j<=len2;j++){
                 
-                if(str1.charAt(idx1-1)==str2.charAt(idx2-1)){
-                    dp[idx1][idx2] = 1 + dp[idx1-1][idx2-1];
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }else{
-                    dp[idx1][idx2]=0+Math.max(dp[idx1-1][idx2],dp[idx1][idx2-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         
         
-        
-        /*Till above tabulation of LCS was used. From here onwards logic for
+         /*Till above tabulation of LCS was used. From here onwards logic for
         getting Supersequence is implemented*/
         
         int len=dp[len1][len2];
@@ -50,32 +47,26 @@ class Solution {
                 index--;
                 i--;
                 j--;
-            }else if(dp[i-1][j] > dp[i][j-1]){
-                
-                ans+= str1.charAt(i-1);
+            }else if(dp[i-1][j]>dp[i][j-1]){
+                ans+=str1.charAt(i-1);
                 i--;
-            }else {
-                
-                ans+= str2.charAt(j-1);
+            }else{
+                ans+=str2.charAt(j-1);
                 j--;
             }
         }
-        
         
         while(i>0){
             ans+=str1.charAt(i-1);
             i--;
         }
         
-        
-         while(j>0){
+        while(j>0){
             ans+=str2.charAt(j-1);
             j--;
         }
         
-        
         String result=new StringBuilder(ans).reverse().toString();
-        
         
         return result;
     }

b7f6368992673a3e63d1814a75c7cf9c722f27b1
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-07 17:55:51 +0530

    Time: 18 ms (73.64%), Space: 49.4 MB (66.28%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -3,23 +3,23 @@ class Solution {
         int len1=s.length();
         int len2=t.length();
         
-        int[][] dp=new int[len1][len2];
+        int[][] dp=new int[len1+1][len2+1];
         
         for(int[] x:dp){
             Arrays.fill(x,-1);    
         }
         
-        return solUtil(s,t,s.length()-1,t.length()-1,dp);
+        return solUtil(s,t,s.length(),t.length(),dp);
     }
     
     public int solUtil(String s, String t,int i,int j,int[][] dp){
         
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         
         if(dp[i][j]!=-1) return dp[i][j];
             
-        if(s.charAt(i)==t.charAt(j)){
+        if(s.charAt(i-1)==t.charAt(j-1)){
             return dp[i][j]=solUtil(s,t,i-1,j-1,dp) + solUtil(s,t,i-1,j,dp);
         }else{
             return dp[i][j]=solUtil(s,t,i-1,j,dp);

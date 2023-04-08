012298de39192a9661ed2682d3e3fecaeaf0eae3
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-09 00:01:58 +0530

    Time: 92 ms (27.85%), Space: 64.6 MB (29.90%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,35 +1,34 @@
-//Memoization
+//Tabulation
 // TC - O(n*2*2) 
-// SC - O(n) + O(n*2*3)
+// SC - O(n*2*3)
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
         
-        int[][][] dp=new int[n][2][3];
+        int[][][] dp=new int[n+1][2][3];
         
-        for(int[][] x:dp){
-            for(int[] y:x){
-                Arrays.fill(y,-1);
+        for(int i=n;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int k=0;k<3;k++){
+                    if(k==0){
+                        dp[i][buy][k]=0;
+                    }else if(i==n){
+                        dp[i][buy][k]=0;
+                    }else{
+                        int profit=0;
+                        if(buy==1){
+                            profit= Math.max(-prices[i] + dp[i+1][0][k], dp[i+1][1][k]);
+                        }else{
+                            profit= Math.max(prices[i] + dp[i+1][1][k-1], dp[i+1][0][k]); 
+                        }
+
+                        dp[i][buy][k]=profit;
+                    }
+                }
             }
         }
         
-        return solUtil(prices,0,1,2,dp);
+        return dp[0][1][2]; 
     }
     
-    public int solUtil(int[] prices,int i,int buy,int k,int[][][] dp){
-        
-        if(k==0) return 0;
-        if(i==prices.length) return 0;
-        
-        if(dp[i][buy][k]!=-1) return dp[i][buy][k];
-        
-        int profit=0;
-        if(buy==1){
-            profit= Math.max(-prices[i] + solUtil(prices,i+1,0,k,dp),solUtil(prices,i+1,1,k,dp));
-        }else{
-            profit= Math.max(prices[i] + solUtil(prices,i+1,1,k-1,dp),solUtil(prices,i+1,0,k,dp));
-        }
-        
-        return dp[i][buy][k]=profit;
-    }
 }

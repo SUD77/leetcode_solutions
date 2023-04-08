568a627eb5203fde86161cd23434802300f33ddb
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-09 00:04:30 +0530

    Time: 100 ms (23.93%), Space: 58.7 MB (57.54%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,34 +1,42 @@
-//Tabulation
+//Space Optimization
 // TC - O(n*2*2) 
-// SC - O(n*2*3)
+// SC - O(1)
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
         
-        int[][][] dp=new int[n+1][2][3];
+        int[][] prev=new int[2][3];
+        
         
         for(int i=n;i>=0;i--){
+            
+            int[][] curr=new int[2][3];
+            
             for(int buy=0;buy<=1;buy++){
+                
                 for(int k=0;k<3;k++){
+                    
                     if(k==0){
-                        dp[i][buy][k]=0;
+                        curr[buy][k]=0;
                     }else if(i==n){
-                        dp[i][buy][k]=0;
+                        curr[buy][k]=0;
                     }else{
                         int profit=0;
                         if(buy==1){
-                            profit= Math.max(-prices[i] + dp[i+1][0][k], dp[i+1][1][k]);
+                            profit= Math.max(-prices[i] + prev[0][k], prev[1][k]);
                         }else{
-                            profit= Math.max(prices[i] + dp[i+1][1][k-1], dp[i+1][0][k]); 
+                            profit= Math.max(prices[i] + prev[1][k-1], prev[0][k]); 
                         }
 
-                        dp[i][buy][k]=profit;
+                        curr[buy][k]=profit;
                     }
                 }
             }
+            prev=curr;
+            
         }
         
-        return dp[0][1][2]; 
+        return prev[1][2]; 
     }
     
 }

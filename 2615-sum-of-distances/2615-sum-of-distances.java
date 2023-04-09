35804c0c99442bc84e1f6e4ea0717e836f0dc959
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-09 16:12:33 +0530

    Time: 35 ms (66.67%), Space: 80 MB (33.33%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -0,0 +1,45 @@
+// Logic - : https://leetcode.com/problems/sum-of-distances/discuss/3395726/Explained-with-images-oror-Easy-to-understand
+
+class Solution {
+    public long[] distance(int[] nums) {
+        
+        long[] ans=new long[nums.length];
+
+        Map<Integer,List<Integer>> mp=new HashMap<>();
+
+
+        for(int i=0;i< nums.length;i++){
+            List<Integer> list = mp.getOrDefault(nums[i], new ArrayList<>());
+            list.add(i);
+            mp.put(nums[i], list);
+        }
+
+        for(Map.Entry<Integer,List<Integer>> ele:mp.entrySet()){
+
+            int key=ele.getKey();
+            List<Integer> tempList=ele.getValue();
+
+            long totalSum=0l;
+
+            for(int x:tempList) totalSum+=(long)x;
+
+            long preSum=0l;
+
+            for(int i=0;i<tempList.size();i++){
+
+                int index=tempList.get(i);
+
+                long postSum=totalSum-preSum-index;
+
+                ans[index]+=(index * (long)i);
+                ans[index]-=(preSum);
+                ans[index]-=(index * (long)(tempList.size() - i -1));
+                ans[index]+=postSum;
+
+                preSum +=index;
+            }
+        }
+
+        return ans;
+    }
+}

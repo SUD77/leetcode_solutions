883a4afbb647e7fc18ea93eebe5a6458e5129d52
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-10 00:34:28 +0530

    Time: 1 ms (79.83%), Space: 40.4 MB (86.37%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,29 +1,36 @@
+//tabulation
+// TC - O(n*2)
+//SC - O(n*2)
+
+
 class Solution {
     public int maxProfit(int[] prices) {
         
         int n=prices.length;
         
-        int[][] dp=new int[n+1][2];
+        int[][] dp=new int[n+2][2];  //took size n+2 as, i is going till i+2
         
-        for(int[] x:dp){
-            Arrays.fill(x,-1);
+        for(int i=n+1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                
+                if(i>=n) dp[i][buy]=0;
+                else{
+                    int profit=0;
+                    if(buy==1){
+                        profit=Math.max(-prices[i] + dp[i+1][0],dp[i+1][1]); 
+                    }else{
+                        
+                        int sellNow=prices[i] + dp[i+2][1];
+                        int sellLater=dp[i+1][0];
+                        profit=Math.max(sellNow,sellLater); 
+                    }
+                    
+                    dp[i][buy]=profit;
+                }
+                
+            }
         }
-        return solUtil(prices,0,1,dp);
+        return dp[0][1];
     }
     
-    public int solUtil(int[] prices,int i,int buy,int[][] dp){
-        
-        if(i>=prices.length) return 0;
-        
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        
-        int profit=0;
-        if(buy==1){
-            profit=Math.max(-prices[i] + solUtil(prices,i+1,0,dp),solUtil(prices,i+1,1,dp));
-        }else{
-            profit=Math.max(prices[i] + solUtil(prices,i+2,1,dp),solUtil(prices,i+1,0,dp));
-        }
-        
-        return dp[i][buy]=profit;
-    }
 }

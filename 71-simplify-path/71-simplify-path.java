5115747e7ddeef98258809eb342ca1ff9d2bf0b1
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-12 11:27:58 +0530

    Time: 4 ms (85.61%), Space: 42.8 MB (42.97%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/simplify-path/discuss/1847526/Best-Explanation-Ever-Possible-oror-Not-a-ClickBait
+
+class Solution {
+    public String simplifyPath(String path) {
+        
+        Stack<String> s=new Stack<>();
+        StringBuilder res=new StringBuilder();
+        
+        String[] p=path.split("/");
+        
+        for(int i=0;i<p.length;i++){
+            
+            if(!s.isEmpty() && p[i].equals("..")) s.pop();
+            else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals("..")) s.push(p[i]);
+        }
+        
+        if(s.isEmpty()) return "/";
+        
+        while(!s.isEmpty()){
+            res.insert(0,s.pop()).insert(0,"/");
+        }
+        
+        return res.toString();
+    }
+}

3a772db0860e785b0deade6d6fa710ea2817c339
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-04-13 22:15:53 +0530

    Time: 65 ms (31.25%), Space: 42.8 MB (35.83%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,9 +1,8 @@
+//Strivers solution 
+
 class Solution {
     public int longestStrChain(String[] words) {
         
-        HashMap<String,Integer> dp=new HashMap<>();
-        
-        int ans=0;
         
         Arrays.sort(words, new Comparator<String>() {
              @Override
@@ -12,22 +11,57 @@ class Solution {
              }
          });
         
-        for(String word:words){
+        
+        int n=words.length;
+        int maxAns=1;
+        
+        
+        //We will fill dp with 1, as indiviudal ele are LIS of size 1
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        
+        for(int index=0;index<n;index++){
             
-            dp.put(word,1);
-            
-            for(int i=0;i<word.length();i++){
+            for(int prevIndex=0;prevIndex<index;prevIndex++){
                 
-                StringBuilder current=new StringBuilder(word);
-                String predecessor = current.deleteCharAt(i).toString();
-               
-                if(dp.containsKey(predecessor))
-                    dp.put(word,Math.max(dp.get(word),dp.get(predecessor)+1));
+               if(checkPossible(words[index],words[prevIndex])
+                 && 1+dp[prevIndex] > dp[index]){
+                   
+                   dp[index]=1+dp[prevIndex];
+                   
+               }
+            }
+           
+            
+            if(dp[index] > maxAns){
+                maxAns=dp[index];
                 
             }
-            ans=Math.max(ans,dp.get(word));
+            
         }
         
-        return ans;
+        
+        return maxAns; 
+    }
+    
+    public boolean checkPossible(String s1,String s2){
+        
+        if(s1.length()!=s2.length()+1) return false;
+        
+        int first=0;
+        int second=0;
+        
+        while(first<s1.length()){
+            
+            if(second<s2.length() && s1.charAt(first)==s2.charAt(second)){
+                first++;
+                second++;
+            }else{
+                first++;
+            }
+        }
+        
+        if(first==s1.length() && second==s2.length()) return true;
+        else return false;
     }
 }

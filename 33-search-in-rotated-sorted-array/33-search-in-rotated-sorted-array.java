6c7745ad8f60be535c61e60cc6a54983b05d6d77
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-05-12 23:48:28 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (29.20%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,34 +1,71 @@
-//Not my code and logic
-
 class Solution {
-    public int search(int[] arr, int target) {
+    public int search(int[] nums, int target) {
         
+        //Will use binary search for this too
+        int minIndex=findMinIndex(nums);
         
-        int low=0;
-        int high=arr.length-1;
+        /*
+        - Basically, we will check if target is less than last ele, 
+        - if it is, than we do bs in minIndex to last ele
+            - as, if is less than last ele, it is surely greater than minIndex
+        - else, will do bs from 0 to minIndex
         
-        while(low<=high){
+        */
+        if(isLessThanRightMost(nums,target)){
+            return binarySearch(nums,target,minIndex,nums.length-1);
+        }else{
+            return binarySearch(nums,target,0,minIndex);
+        }
+    }
+    
+    public boolean isLessThanRightMost(int[] nums,int target){
+        return target<=nums[nums.length-1];
+    }
+    
+    public int findMinIndex(int[] nums){
+        
+        int low=-1;
+        int high=nums.length;
+        
+        while(low+1<high){
             
-            int mid=(low+high)>>1;
+            int mid=low + (high-low)/2;
             
-            if(arr[mid]==target)
-                return mid;
-            
-            //this if else check if left half is sorted or right
-            if(arr[low]<=arr[mid]){
-                
-                if(arr[low]<=target && arr[mid]>=target)
-                    high=mid-1;
-                else
-                    low=mid+1;
+            if(isLessThanOrEqual(nums[mid],nums)){
+                high=mid;
             }else{
-                if(arr[mid]<=target && target<=arr[high])
-                    low=mid+1;
-                else
-                    high=mid-1;
+                low=mid;
             }
         }
         
-        return -1;
+        return high;
+    }
+    
+    public boolean isLessThanOrEqual(int ele,int[] nums){
+        return ele<=nums[nums.length-1];
+    }
+    
+    
+    public int binarySearch(int[] arr,int target,int left,int right){
+        
+        int low=left-1;
+        int high=right;
+        
+        while(low+1<high){
+            
+            int mid=low + (high - low)/2;
+            
+            if(isGreaterOrEqual(arr[mid],target)){
+                high=mid;
+            }else{
+                low=mid;
+            }
+        }
+        
+        return arr[high]==target ? high:-1;
+    }
+    
+    public boolean isGreaterOrEqual(int x,int y){
+        return x>=y;
     }
 }

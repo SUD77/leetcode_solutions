36b74480d038c1d8704fb71ef28f5866652c3047
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-07-08 15:38:31 +0530

    Time: 38 ms (55.68%), Space: 59.9 MB (15.91%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/put-marbles-in-bags/discuss/3111736/JavaC%2B%2BPython-3-Approachs-Best-O(n)
+
+class Solution {
+     public long putMarbles(int[] A, int k) {
+        int n = A.length - 1;
+        long B[] = new long[n], res = 0;
+        for (int i = 0; i < B.length; i++) {
+            B[i] = A[i] + A[i + 1];
+        }
+        Arrays.sort(B);
+        for (int i = 0; i < k - 1; i++) {
+            res += B[n - 1 - i] - B[i];
+        }
+        return res;
+    }
+}

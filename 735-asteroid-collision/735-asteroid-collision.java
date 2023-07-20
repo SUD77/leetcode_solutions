99cd328c2d5c48e37ead3c6091165d9df516c4c1
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-07-21 02:11:16 +0530

    Time: 11 ms (65.76%), Space: 44.2 MB (50.54%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/asteroid-collision/discuss/193403/Java-easy-to-understand-solution
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
+        for(int i: asteroids){
+            if(i > 0){
+                s.push(i);
+            }else{// i is negative
+                while(!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(i)){
+                    s.pop();
+                }
+                if(s.isEmpty() || s.peek() < 0){
+                    s.push(i);
+                }else if(i + s.peek() == 0){
+                    s.pop(); //equal
+                }
+            }
+        }
+        int[] res = new int[s.size()];   
+        for(int i = res.length - 1; i >= 0; i--){
+            res[i] = s.pop();
+        }
+        return res;
+    }
+}
+
+

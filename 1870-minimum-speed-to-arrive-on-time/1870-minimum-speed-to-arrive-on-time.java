129f93a53ccccd92c4fcb84059720ccdc0761310
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-07-26 14:36:29 +0530

    Time: 200 ms (69.55%), Space: 59.1 MB (38.41%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/minimum-speed-to-arrive-on-time/discuss/1224643/Java-binary-search-explained
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int n = dist.length;
+        int min = 1, max = 10000000;
+        int ans = -1;
+        while(min <= max){
+            int mid = (max + min)/2;
+            double sum = 0;
+            for(int i = 0; i<n-1; ++i){
+                sum += Math.ceil( ( (double) dist[i]) /mid);
+            }
+            sum = sum + ( ( (double) dist[n-1]) /mid);
+            if(sum > hour){
+                min = mid+1;
+            }else{
+                ans = mid;
+                max = mid-1;
+            }
+        }
+        return ans;
+    }
+}

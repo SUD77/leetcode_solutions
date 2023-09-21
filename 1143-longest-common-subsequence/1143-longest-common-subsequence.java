51697a5e8b250dfa20a29ed066cccadcc2d32fa0
SUD77 <jaiswalsudhanshu20@gmail.com>
2023-09-21 16:59:29 +0530

    Time: 53 ms (5.07%), Space: 48.1 MB (80.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,48 +1,31 @@
-//Printing LCS ..................
-//Tabu
-//TC =  O(n1 x n2)
-//SC =  O(n x m )dp[][]
-
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n1 = text1.length();
-        int n2 = text2.length();
         
-        int dp[][] = new int[n1+1][n2+1];
-        //Initialisation
-        for(int i=0; i<=n1; i++) dp[i][0] = 0;
-        for(int j=0; j<=n2; j++) dp[0][j] = 0;  
+        int len1=text1.length();
+        int len2=text2.length();
         
-        for(int i=1; i<=n1; i++){
-            for(int j=1; j<=n2; j++){
-                if(text1.charAt(i-1) == text2.charAt(j-1))  
-                    dp[i][j] = 1 + dp[i-1][j-1]; 
-                else{
-                    dp[i][j] = 0 + Math.max(dp[i][j-1], dp[i-1][j]) ;
-                }
-            }
+        int[][] dp=new int[len1+1][len2+1];
+        
+        for(int[] x:dp){
+            Arrays.fill(x,-1);
         }
-        //Printing LCS
-        int i=n1;
-        int j = n2;
-        String str = "";
         
-        while(i>=1 && j>=1){
-            if(text1.charAt(i-1) == text2.charAt(j-1)){
-                str += text1.charAt(i-1); 
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                i--;                
-            }
-            else{
-                j--;
-            }
+        return solUtil(0,0,text1,text2,dp);
+    }
+    
+    public int solUtil(int index1,int index2,String text1,String text2,int[][] dp){
+        
+        if(index1>=text1.length() || index2>=text2.length()) return 0;
+        
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        
+        int pick=0;
+        if(text1.charAt(index1)==text2.charAt(index2)){
+            pick=1+solUtil(index1+1,index2+1,text1,text2,dp);
         }
-        StringBuilder sb = new StringBuilder(str);
-        String lcs = sb.reverse().toString();
-        System.out.println("LCS String = " + lcs);
-        return dp[n1][n2]; 
+        
+        int notPick=Math.max(solUtil(index1,index2+1,text1,text2,dp),solUtil(index1+1,index2,text1,text2,dp));
+        
+        return dp[index1][index2]=Math.max(pick,notPick);
     }
 }

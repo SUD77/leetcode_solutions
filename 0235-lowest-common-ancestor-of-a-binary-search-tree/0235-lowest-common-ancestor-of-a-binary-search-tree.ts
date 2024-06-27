060a865b8762db475ce7ec17e43fe2367fe9ab1b
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-06-27 23:30:47 +0530

    Time: 82 ms (33.39%), Space: 59.4 MB (94.30%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	return solUtil(root,p,q);
+};
+
+function solUtil(root:TreeNode | null, p: TreeNode | null, q:TreeNode | null): TreeNode | null{
+    
+    if(root.val==p.val || root.val==q.val) return root;
+    
+    if(root.val>p.val && root.val<q.val) return root;
+    if(root.val>q.val && root.val<p.val) return root;
+    
+    if(root.val>q.val && root.val>p.val) return solUtil(root.left,p,q);
+    
+    return solUtil(root.right,p,q);
+}

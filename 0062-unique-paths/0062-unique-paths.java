c5d0afa77475a5703fe743bf11b17f4eb0775494
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-06-30 16:58:17 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (26.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,31 @@
-//Memoization 
-//TC - O((m*n))
-// SC - O(m+n) + O(m*n)
+//Tabulation - Bottom up 
+//TC - O(m*n)
+//SC - O(m*n)
 
 class Solution {
     public int uniquePaths(int m, int n) {
         
         int[][] dp=new int[m+1][n+1];
         
-        for(int[] x:dp){
-            Arrays.fill(x,-1);
+        
+        dp[0][0]=1;
+        
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
         }
         
-        return solUtil(m-1,n-1,m,n,dp);
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
+        }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+             
+                dp[i][j]=dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
     
-    public int solUtil(int i,int j,int m,int n,int[][] dp){
-        
-        if(i<0 || j<0) return 0; 
-        
-        if(i==0 && j==0) return 1;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int left=solUtil(i,j-1,m,n,dp);
-        int up=solUtil(i-1,j,m,n,dp);
-        
-        
-        return dp[i][j]=left + up;
-        
-        
-    }
 }

8bcde490e5ddc22305bb71decca7cc365d313086
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-08-20 22:12:25 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (31.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,29 @@
-//Space Optimization  
-//TC - O(m*n)
-//SC - O(n)
-
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int[] prev=new int[n+1];
-        Arrays.fill(prev,1);
+        int[][] dp=new int[m+1][n+1];
         
-        
-        for(int i=1;i<m;i++){
-            int[] curr=new int[n+1];
-            curr[0]=1;
-            for(int j=1;j<n;j++){
-             
-                curr[j]=curr[j-1] + prev[j];
-            }
-            
-            prev=curr;
+        for(int[] x:dp){
+            Arrays.fill(x,-1);
         }
         
-        return prev[n-1];
+        return solUtil(0,0,m,n,dp);
+        
     }
     
+    public int solUtil(int i,int j,int m,int n,int[][] dp){
+        
+        if(i>m || j>n) return 0;
+        
+        if(i==m-1 && j==n-1) return 1;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        
+        int down=solUtil(i+1,j,m,n,dp);
+        int right=solUtil(i,j+1,m,n,dp);
+        
+        
+        return dp[i][j]=down + right;
+    }
 }
